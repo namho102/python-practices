8bc3491ee6a713cbf142476d0b1e2643193a21fe
Hoang Nam <namhoang.td09@gmail.com>
2016-04-24 19:35:52 +0700

    add codes

---
+++ aiohttp/client.py
@@ -0,0 +1,10 @@
+import asyncio
+
+@asyncio.coroutine
+def speak_async():  
+    print('OMG asynchronicity!')
+
+loop = asyncio.get_event_loop()  
+loop.run_until_complete(speak_async())  
+loop.close()  
+
